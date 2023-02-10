fe060533f7baf292205e8b037f39a688b12907ba
Tiago Damasceno <devtiago01@gmail.com>
2023-02-10 02:45:25 -0300

    v 0.2.1

--- mp3.py
+++ mp3.py
@@ -1,18 +1,25 @@
 import PySimpleGUI as sg
-print(oi)
-sg.theme('black')
+import os
+from play import get_diretorios, play_sound, pause_sound, unpause, stop_sound, is_sound_playing
 
-layout = [[sg.Text('muito bom')],
-[sg.Text('saber python'), sg.InputText()],
-[sg.Button('ok'), sg.Button('cancel')]]
+sg.theme('DarkPurple5')
+nome_da_musica = [[sg.Text(text='press play..', justification = 'center', background_color = 'blue',
+             text_color= 'black', size = (200.0), font = 'tahoma', key = 'song_name')]] 
 
-window = sg.Window('acertei', layout)
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'cancel':
-        break
-print('you entered', values=[0])
 
-window.close()
+
+# 1 criar um player de musica
+    #1.1 criar uma playlist
+    #1.2 fazer a play list tocar
+    #1.3 transportar a playlist para a aba interativa
+
+# 2 criar uma aba para aparecer o player
+    #2.1 criar uma aba grande
+    #2.2 colocar uma iamgem da banda no meio
+    #2.3 criar botões play e pause
+
+# 3 fazer a aba dar play e pause na musica 
+    # Fazer o botão dar play
+    #fazer o botão dar pause
 
     
--- play.py
+++ play.py
@@ -1,2 +1,31 @@
 import os
 from pygame import mixer
+mixer.init
+
+def get_diretorios(directory):
+    directories = []
+    for (root, dir, files) in os.walk(directory):
+        for file in files:
+            directories.append(root + os.sep + file)
+
+    return directories
+
+
+
+def play_sound(sound_path):
+    mixer.music.load(sound_path)
+    mixer.music.play()
+
+def stop_sound():
+    mixer.music.stop
+
+def pause_sound():
+    mixer.music.pause()
+
+def unpause ():
+    mixer.music.unpause()
+
+def is_sound_playing():
+    if mixer.music.get_busy() == True:
+        return True
+    return False
